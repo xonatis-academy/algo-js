d12f94f8b737eb6cd63506cc51afd60739cf6023
michael <nguyen.hoang.michael@gmail.com>
2020-10-13 15:27:23 +0200

    Adding classes

---
+++ classes.js
@@ -0,0 +1,15 @@
+class User {
+    firstname;
+    age;
+}
+
+let user = new User();
+user.firstname = 'Jean';
+user.age = 32;
+
+let user2 = new User();
+user2.firstname = 'Sophie';
+user2.age = 41;
+
+console.log(user);
+console.log(user2);
